6839e29959368dc3598051ac2a0c2eb1866f410d
Ivanov Mikhail <wannatohelp@gmail.com>
2025-01-22 19:19:16 +0300

    Add files via upload

---
+++ generated_files/generated_test2.py
@@ -0,0 +1,45 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { AuthService } from 'src/app/services/auth.service';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.css']
+})
+export class LoginComponent implements OnInit {
+
+  loginForm: FormGroup;
+  submitted = false;
+  error = '';
+
+  constructor(private formBuilder: FormBuilder, private router: Router, private authService: AuthService) { }
+
+  ngOnInit(): void {
+    this.loginForm = this.formBuilder.group({
+      username: ['', Validators.required],
+      password: ['', Validators.required]
+    });
+  }
+
+  get f() { return this.loginForm.controls; }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.authService.login(this.f.username.value, this.f.password.value).subscribe(
+      data => {
+        this.router.navigate(['/home']);
+      },
+      err => {
+        this.error = err;
+      }
+    );
+  }
+
+}
---
+++ generated_files/test3_fixed.py
@@ -0,0 +1,5 @@
+:
+    a, b = 0, 1
+    while True:
+        yield a
+        a, b = b, a + b
